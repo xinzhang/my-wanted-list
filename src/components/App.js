4da8211e77223c8c242ac4058d62858a35ce1011
Xin Zhang <Xin.Zhang@ausmaq.com.au>
2017-01-27 16:18:52 +1100

    add user (show screen)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
@@ -8,16 +7,47 @@ import {getWantedList} from '../actions/get_wanted_list';
 
 import LoadingSpinner from './LoadingSpinner';
 import WantedCard from './WantedCard';
+import AddUserModal from './AddUserModal'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+          openModal: false,
+          newPersonName: '',
+          newPersonReason: '',
+          newPersonReward: '',
+          newPersonEyes: '',
+          newPersonNose: '',
+          newPersonMouth: '',
+          newPersonSkin: '#CE96FF'
+    }
 
     this.toggleModalState = this.toggleModalState.bind(this);
   }
 
   toggleModalState() {
+    if(this.state.openModal) {
+      this.clearFormAndCloseModal();
+    } else {
+      this.setState({
+        openModal: true
+      })
+    }
   }
+
+  clearFormAndCloseModal() {
+    this.setState({
+      newPersonName: '',
+      newPersonReason: '',
+      newPersonReward: '',
+      newPersonEyes: 1,
+      newPersonNose: 1,
+      newPersonMouth: 1,
+      newPersonSkin: '#CE96FF',
+      openModal: false
+    });
+}
 
   renderUsers() {
       if(this.props.wantedPeople) {
@@ -51,6 +81,20 @@ class App extends React.Component {
             </div>
           </div>
         </div>
+        <AddUserModal
+
+
+          name={this.state.newPersonName}
+          reason={this.state.newPersonReason}
+          reward={this.state.newPersonReward}
+          eyes={this.state.newPersonEyes}
+          nose={this.state.newPersonNose}
+          mouth={this.state.newPersonMouth}
+          skinTone={this.state.newPersonSkin}
+          
+          open={this.state.openModal}
+          close={this.toggleModalState}
+          />
       </div>
     );
   }
